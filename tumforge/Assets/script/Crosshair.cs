76058be51be378055f90f72c40a6d6481a1a06b5
Mr.Chewatch <thannawartsinn@gmail.com>
2020-02-04 05:53:06 +0700

    Tumforge0.3.1 (Aiming ready)
    -cursor lag (fixed)
    -velocity bug (fixed)
    -flip
    -code optimized

--- tumforge/Assets/script/Crosshair.cs
+++ tumforge/Assets/script/Crosshair.cs
@@ -24,10 +24,6 @@ public class Crosshair : MonoBehaviour
     void Update()
     {
         CrossHairMovement();
-        
-        
-        
-        
     }
 
     private void CrossHairMovement()
--- tumforge/Assets/script/playermovement.cs
+++ tumforge/Assets/script/playermovement.cs
@@ -4,197 +4,234 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour
 {
-    [SerializeField] private float m_JumpForce = 400f;                         
-    [Range(0, 100f)] [SerializeField] private float m_RunSpeed = 40f;          
-    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;  
-    [SerializeField] private bool m_AirControl = false;                       
-    [SerializeField] private LayerMask m_WhatIsGround;                          
-    [SerializeField] private Transform m_GroundCheck;
-    //--------------------------------------------------
-    const float k_GroundedRadius = .2f; 
-    private bool m_Grounded;           
-    private Rigidbody2D m_Rigidbody2D;
-    private bool m_FacingRight = true;  
-    private Vector3 m_Velocity = Vector3.zero;
-    //--------------------------------------------------
-    float horizontalMove = 0f;
-    bool jump = false;
-    bool dash = false;
-    public GameObject canflip;
-    public SpriteRenderer spriteRen;
-
-    public int jumplimit = 2;
-    int jumpnumber;
-    private float rotatez = 0.0f;
-    public int dashlimit = 2;
-    int dashnumber;
-    public float dashtimeValue = 0.1f;
-    float dashtime;
-    public float dashvelocity = 120;
-    bool isdashing = false;
+    //------------------Property------------------------
+    [SerializeField] private float P_JumpForce = 400f;
+    [Range(0, 100f)] [SerializeField] private float P_RunSpeed = 40f;
+    [Range(0, .3f)] [SerializeField] private float P_MovementSmoothing = .05f;
+    [SerializeField] private bool P_AirControl = false;
+    private Crosshair P_crossHair;
+    private GameObject P_Camera;
+    //---------------characterstate---------------------
+    [SerializeField] private LayerMask S_WhatIsGround;
+    [SerializeField] private Transform S_GroundCheck;
+    const float S_GroundedRadius = .2f;
+    private bool S_Grounded;
+    private Rigidbody2D S_Rigidbody2D;
+    private bool S_FacingRight = true;
+    private Vector3 S_Velocity = Vector3.zero;
+    //-------------------movement-----------------------
+    float M_horizontalMove = 0f;
+    bool M_jump = false;
+    bool M_dash = false;
+    public GameObject M_canflip;
+    //public SpriteRenderer spriteRen; //not in use
+    //--------------------jump--------------------------
+    public int J_jumplimit = 2;
+    int J_jumpnumber;
+    //-------------------dashing------------------------
+    public int D_dashlimit = 2;
+    int D_dashnumber;
+    public float D_dashtimeValue = 0.1f;
+    float D_dashtime;
+    public float D_dashvelocity = 120;
+    bool D_isdashing = false;
     Vector2 speed_before_dash;
-    public GameObject AttackWindow;
-    public float A_atttackDuration = 0f;
-    public float A_attackSpeed = 0f;
-    public float A_attackVelocity = 0f;
-    Vector2 A_direction;
-    public GameObject swoosh;
+    //-------------------attacking-----------------------
+    bool A_slash = false;
+    public GameObject A_SWHOOSH;
+    private float A_rotaion;
+    int A_slashlimit = 2;
+    int A_slashnumber;
+    public float A_slashtimevalue = 0.1f;
+    float A_slashtime;
+    public float A_slashvelocity = 120;
+    bool A_isAttacking = false;
+    Vector2 speed_before_slash;
+
+
+
+
     private void Awake()
     {
-        m_Rigidbody2D = GetComponent<Rigidbody2D>();
-        
-        
+        S_Rigidbody2D = GetComponent<Rigidbody2D>();
+        P_Camera = GameObject.Find("Main Camera");
+        P_crossHair = P_Camera.GetComponent<Crosshair>();
     }
 
     private void FixedUpdate()
     {
         GroundCheck();
         OngroundEvent();
-        movement();
-        jump = false;
-        dash = false;
+        character();
+        Swhoosh_movement();
+        M_jump = false;
+        M_dash = false;
+        A_slash = false;
     }
 
     public void Move(float move, bool jump, bool dash)
     {
-        dashtime -= Time.deltaTime;
+        D_dashtime -= Time.deltaTime;
 
-        if (m_Grounded || m_AirControl)
+        if (S_Grounded || P_AirControl)
         {
-            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+            Vector3 targetVelocity = new Vector2(move * 10f, S_Rigidbody2D.velocity.y);
 
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+            S_Rigidbody2D.velocity = Vector3.SmoothDamp(S_Rigidbody2D.velocity, targetVelocity, ref S_Velocity, P_MovementSmoothing);
 
-            if (move > 0 && !m_FacingRight)
+            if (move > 0 && !S_FacingRight)
             {
                 Flip();
             }
-            else if (move < 0 && m_FacingRight)
+            else if (move < 0 && S_FacingRight)
             {
                 Flip();
             }
         }
-        if (jumpnumber > 0 && jump)
+        if (J_jumpnumber > 0 && jump)
         {
-            m_Rigidbody2D.velocity = new Vector2(m_Rigidbody2D.velocity.x, 0);
-            m_Grounded = false;
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-            jumpnumber -= 1;
+            S_Rigidbody2D.velocity = new Vector2(S_Rigidbody2D.velocity.x, 0);
+            S_Grounded = false;
+            S_Rigidbody2D.AddForce(new Vector2(0f, P_JumpForce));
+            J_jumpnumber -= 1;
         }
         //------------------dash-----------------
-        if (dashnumber > 0 && dash)
+        if (D_dashnumber > 0 && dash)
         {
-            speed_before_dash = new Vector2(m_Rigidbody2D.velocity.x, m_Rigidbody2D.velocity.y);
-            dashnumber -= 1;
-            isdashing = true;
-            dashtime = dashtimeValue;
+            speed_before_dash = new Vector2(S_Rigidbody2D.velocity.x, S_Rigidbody2D.velocity.y);
+            D_dashnumber -= 1;
+            D_isdashing = true;
+            D_dashtime = D_dashtimeValue;
         }
-        if (dashtime > 0 && isdashing)
+        if (D_dashtime > 0 && D_isdashing)
         {
-            if (m_FacingRight)
+            if (S_FacingRight)
             {
-                m_Rigidbody2D.velocity = new Vector2(dashvelocity, 0);
+                S_Rigidbody2D.velocity = new Vector2(D_dashvelocity, 0);
             }
             else
             {
-                m_Rigidbody2D.velocity = new Vector2(- dashvelocity, 0);
+                S_Rigidbody2D.velocity = new Vector2(-D_dashvelocity, 0);
             }
         }
-        if (dashtime == 0.1)
+        if ((D_dashtime < 0.07) && (D_dashtime > 0.0001) && (D_isdashing = true))
         {
-            m_Rigidbody2D.velocity = speed_before_dash;
-            isdashing = false;
+            D_isdashing = false;
+            S_Rigidbody2D.velocity = speed_before_dash;
         }
-        if (dashtime < 0)
+        if (D_dashtime < 0)
         {
-            dashtime = 0;
+            D_dashtime = 0;
         }
     }
 
     private void Flip()
     {
-        m_FacingRight = !m_FacingRight;
+        S_FacingRight = !S_FacingRight;
 
-        Vector3 theScale = canflip.transform.localScale;
+        Vector3 theScale = M_canflip.transform.localScale;
         theScale.x *= -1;
-        canflip.transform.localScale = theScale;
+        M_canflip.transform.localScale = theScale;
 
-        spriteRen.flipX = !spriteRen.flipX;
+        //spriteRen.flipX = !spriteRen.flipX;
     }
 
     void Update()
     {
-        print(isdashing);
-        print(dashtime);
-        horizontalMove = Input.GetAxisRaw("Horizontal") * m_RunSpeed;
+        print(A_rotaion);
+        M_horizontalMove = Input.GetAxisRaw("Horizontal") * P_RunSpeed;
 
         if ((Input.GetButtonDown("Jump")))
         {
-            jump = true;
+            M_jump = true;
         }
 
         if (Input.GetAxisRaw("Horizontal") != 0 && Input.GetKeyDown(KeyCode.LeftShift))
         {
-            dash = true;
+            M_dash = true;
         }
-        if (A_atttackDuration <= 0)
-        {
-            swoosh.SetActive(false);
-            if (Input.GetMouseButton(0))
-            {
-                A_atttackDuration = A_attackSpeed;
-                playeratk();
-            }
-            
-            
 
-        }
-        else
+        if (Input.GetMouseButtonDown(0))
         {
-            A_atttackDuration -= Time.deltaTime;
-
+            A_slash = true;
         }
     }
 
-   void playeratk()
-    {
-        GameObject Thecam = GameObject.Find("Main Camera");
-        Crosshair crossHair = Thecam.GetComponent<Crosshair>();
-        rotatez = crossHair.rotationz;
-        A_direction = crossHair.direction;
-        swoosh.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -rotatez);
-        swoosh.SetActive(true);
-        m_Rigidbody2D.velocity = A_direction * A_attackVelocity;
-        
 
-    }
-    void movement()
+    void character()
     {
-        Move(horizontalMove * Time.fixedDeltaTime, jump, dash);
+        Move(M_horizontalMove * Time.fixedDeltaTime, M_jump, M_dash);
+        Slash(A_slash);
     }
 
     void GroundCheck()
     {
-        bool wasGrounded = m_Grounded;
-        m_Grounded = false;
+        bool wasGrounded = S_Grounded;
+        S_Grounded = false;
 
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-        for (int i = 0; i<colliders.Length; i++)
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(S_GroundCheck.position, S_GroundedRadius, S_WhatIsGround);
+        for (int i = 0; i < colliders.Length; i++)
         {
             if (colliders[i].gameObject != gameObject)
             {
-                m_Grounded = true;
+                S_Grounded = true;
             }
         }
     }
 
     void OngroundEvent()
     {
-        if (m_Grounded)
+        if (S_Grounded)
         {
-            jumpnumber = jumplimit;
-            dashnumber = dashlimit;
+            J_jumpnumber = J_jumplimit;
+            D_dashnumber = D_dashlimit;
+            A_slashnumber = A_slashlimit;
+        }
+
+    }
+
+    void Swhoosh_movement()
+    {
+        A_rotaion = P_crossHair.rotationz;
+        A_SWHOOSH.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -A_rotaion);
+    }
+
+    void Slash(bool slash)
+    {
+        A_slashtime -= Time.deltaTime;
+
+        if (A_slashnumber > 0 && slash)
+        {
+            speed_before_slash = new Vector2(S_Rigidbody2D.velocity.x, S_Rigidbody2D.velocity.y);
+            A_SWHOOSH.SetActive(true);
+            A_slashnumber -= 1;
+            A_isAttacking = true;
+            A_slashtime = A_slashtimevalue;
+        }
+        if (A_slashtime > 0 && A_isAttacking)
+        {
+            S_Rigidbody2D.velocity = P_crossHair.direction * A_slashvelocity;
+
+            if (S_FacingRight && (A_rotaion <= 0))
+            {
+                Flip();
+            }
+            else if (!S_FacingRight && (A_rotaion >= 0))
+            {
+                Flip();
+            }
+        }
+        if ((A_slashtime < 0.07) && (A_slashtime > 0.0001) && (A_isAttacking = true))
+        {
+            A_isAttacking = false;
+            S_Rigidbody2D.velocity = speed_before_slash;
+            A_SWHOOSH.SetActive(false);
+        }
+        if (A_slashtime < 0)
+        {
+            A_slashtime = 0;
         }
     }
 
 }
+
