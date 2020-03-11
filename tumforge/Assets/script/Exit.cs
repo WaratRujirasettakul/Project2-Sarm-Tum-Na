e4061e531d1dca84bdba19a7e2801eb00205d8e5
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-03-11 23:35:49 +0700

    Beam Update 1.0.5
    Build: Engine_Proof
    -Exit script
    -Teleport script
    -Menu Linking

---
+++ tumforge/Assets/script/Exit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// Script for portal to exit to next level
+
+public class Exit : MonoBehaviour
+{
+    public string CollisionWithTag;
+    public string SceneToLoad;
+    public object ColWithThis;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (CollisionWithTag.Contains(CollisionWithTag))
+        {
+                Application.LoadLevel(SceneToLoad);
+        }
+    }
+}
