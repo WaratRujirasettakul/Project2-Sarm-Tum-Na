675508696a027783722cb182aaa2c188b90f36fd
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-03-22 23:55:02 +0700

    Beam Update 1.0.7.5
    lolz

---
+++ tumforge/Assets/script/Player/VerticalPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlatform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    public float waitTime;
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetAxisRaw("Vertical") == -1)
+        {
+
+        }
+    }
+}
+//Get axis vertical. Hold = -1 sec,open timer 0.5, else reset timer, If timer = pass = release the player down.
