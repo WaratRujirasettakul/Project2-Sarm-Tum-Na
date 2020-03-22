edaed27374f450e590e1389d7c0b1aae92375a29
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-03-23 00:45:38 +0700

    Beam Update 1.0.7.5.5
    LOLX2

--- tumforge/Assets/script/Player/VerticalPlatform.cs
+++ tumforge/Assets/script/Player/VerticalPlatform.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class VerticalPlatform : MonoBehaviour
 {
     private PlatformEffector2D effector;
-    public float waitTime;
+    public float waitTimer;
     void Start()
     {
         effector = GetComponent<PlatformEffector2D>();
@@ -14,6 +14,24 @@ public class VerticalPlatform : MonoBehaviour
     void Update()
     {
         if(Input.GetAxisRaw("Vertical") == -1)
+        {
+            waitTimer = 0.5f;
+        }
+        if(Input.GetAxisRaw("Vertical") == -1)
+        {
+            if(waitTimer <=0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTimer = 0.5f;
+
+            }
+            else
+            {
+                waitTimer -= Time.deltaTime;
+            }
+        }
+
+        //if (Input.GetAxisRaw("Jump"))
         {
 
         }
