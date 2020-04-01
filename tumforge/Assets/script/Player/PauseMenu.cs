e60534e58d0b5c5a6d55a7afb2c07b8a1a2e871f
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-04-01 19:36:24 +0700

    Beam Update 1.0.8
    -Audio systems are ready.
    -Music volume setting.
    -Sound Effects volume setting.

---
+++ tumforge/Assets/script/Player/PauseMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPause = false;
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    void ResumeToMenu()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    public void LevelSelect()
+    {
+        ResumeToMenu();
+        Application.LoadLevel("LevelSelect");
+    }
+    public void ReturnToMenu()
+    {
+        ResumeToMenu();
+        Application.LoadLevel("MenuScene");
+    }
+}
