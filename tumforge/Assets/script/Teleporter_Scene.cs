3f8c3fa4cb2f867b18c52074bcc051eb033705cd
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-04-07 04:40:07 +0700

    Beam Update 1.0.8.5
    Place holder
    -Level select
    -Win
    Scene Teleporter
    -Door destroyable

--- tumforge/Assets/script/Teleporter_Scene.cs
+++ tumforge/Assets/script/Teleporter_Scene.cs
@@ -5,15 +5,19 @@ using UnityEngine.SceneManagement;
 
 // Script for portal to exit to next level
 
-public class Exit : MonoBehaviour
+public class Teleporter_Scene : MonoBehaviour
 {
-    public string CollisionWithTag;
     public string SceneToLoad;
-    public object ColWithThis;
+    public string TagList = "Player";
+    GameObject player;
 
+    private void Start()
+    {
+        player = GameObject.Find("player");
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (CollisionWithTag.Contains(CollisionWithTag))
+        if (TagList.Contains(TagList))
         {
                 Application.LoadLevel(SceneToLoad);
         }
