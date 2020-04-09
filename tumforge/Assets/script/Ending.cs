90b7a3077aa8aadb310e0e7b36678a4a4f9db7ea
Pepperboi <54356536+Pepperboi@users.noreply.github.com>
2020-04-09 12:37:29 +0700

    Death able

---
+++ tumforge/Assets/script/Ending.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Ending : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("player");
+        if(player == null)
+        {
+            SceneManager.LoadScene(8);
+        }
+    }
+}
