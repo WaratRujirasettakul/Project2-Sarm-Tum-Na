58deb5d344ad2cef5bbd1ba09b9371b6a062927e
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-04-30 04:06:57 +0700

    Beam Update 1.0.9.7.6.3
    New decorations

---
+++ tumforge/Assets/script/UI/LoadingMenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingMenuScript : MonoBehaviour
+{
+    [Header("Canvas")]
+    public GameObject loadingUI;
+
+    [Header("Scene")]
+    Scene current_Scene;
+    string currentSceneName;
+    int nextSceneNumber;
+
+    void Start()
+    {
+        current_Scene = SceneManager.GetActiveScene();
+        currentSceneName = current_Scene.name;
+        nextSceneNumber = gameObject.GetComponent<WinLose_Script>().sceneNumber;
+            //current_Scene.buildIndex + 1;
+    }
+
+
+
+
+
+
+
+
+
+
+    public void continueLevel()
+    {
+        StartCoroutine(LoadAsynchronously());
+    }
+
+    IEnumerator LoadAsynchronously()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(nextSceneNumber);
+
+        while (!operation.isDone == false)
+        {
+            Debug.Log (operation.progress);
+
+            yield return null;
+        }
+    }
+}
