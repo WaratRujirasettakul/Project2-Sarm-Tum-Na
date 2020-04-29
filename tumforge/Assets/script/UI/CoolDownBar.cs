4e42f44f3941024eeebea4276e7a7e79c8c0c4d2
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-04-30 02:12:47 +0700

    Beam Update 1.0.9.7.6.3
    Stair

--- tumforge/Assets/script/UI/CoolDownBar.cs
+++ tumforge/Assets/script/UI/CoolDownBar.cs
@@ -1,8 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CoolDownBar : MonoBehaviour
 {
-    
+    [Header("Slider")]
+    private Slider slider;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+
+
 }
