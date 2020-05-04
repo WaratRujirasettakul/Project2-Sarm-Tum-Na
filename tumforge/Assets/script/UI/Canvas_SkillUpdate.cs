0847bfbd9ff56db620533ae51c9040dfa9736d29
Supakorn  Suwannarit <54356230+SupakornIDD@users.noreply.github.com>
2020-05-04 23:34:58 +0700

    1.1.5
    Canvas done

---
+++ tumforge/Assets/script/UI/Canvas_SkillUpdate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Canvas_SkillUpdate : MonoBehaviour
+{
+    public GameObject timeSlowButton;
+    public GameObject timeStopButton;
+    Scene current_Scene;
+    string currentSceneName;
+    void Start()
+    {
+        current_Scene = SceneManager.GetActiveScene();
+        currentSceneName = current_Scene.name;
+    }
+
+    void Update()
+    {
+        if (currentSceneName != "SC_MainMenuV.2")
+        {
+            if (Code_staticDataHolder.skillcode == 1)
+            {
+                timeSlowButton.SetActive(true);
+                timeStopButton.SetActive(false);
+            }
+            else if (Code_staticDataHolder.skillcode == 2)
+            {
+                timeSlowButton.SetActive(false);
+                timeStopButton.SetActive(true);
+            }
+        }
+    }
+}
